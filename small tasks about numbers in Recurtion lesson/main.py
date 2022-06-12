a3409421b923a8f4ffb8b1bdf319efcb3abf63d2
oliabn <olusiabn@gmail.com>
2022-06-12 16:01:10 +0200

    Add Home Work Tasks

--- small tasks about numbers in Recurtion lesson/main.py
+++ small tasks about numbers in Recurtion lesson/main.py
@@ -34,8 +34,75 @@ def num_sum(num, sum = 0):
     return num_sum(num=num, sum=sum)
 
 
+def print_n_to_1(n):
+    if n == 0:
+        return 0
+    print(n)
+    n -= 1
+    print_n_to_1(n)
+
+
+"""Напишите рекурсивную функцию, чтобы сгенерировать и вернуть список от 1 до N."""
+
+
+def list_generator(n, num_list):
+    if not num_list:                        # Визначаемо перший раз список
+        num_list = [1]
+    if len(num_list) == n:                  # Умова виходу з рекурсії
+        return num_list
+    num_list.append(len(num_list)+1)        # Додаємо єлемент списку
+    return(list_generator(n, num_list))     # Рекурсія
+
+
+"""Напишите рекурсивную функцию, чтобы сгенерировать и вернуть список от 1 до N.
+Аргументом функции является только N"""
+
+
+def list_generator2(n):
+    if n == 0:                          # Умова виходу з рекурсії
+        return []
+    return [n] + list_generator2(n-1)   # Рекурсія
+
+"""Напишите функцию, которая рекурсивно ищет в сложном объекте значение. Сложный 
+объект — это будет список списков списков и т.д. Например, [1, 2, [3, 4, [5, [6, []]]]]. Функция 
+должна рекурсивно заходить внутрь вложенных массивов, а если это другой тип данных 
+игнорировать его"""
+
+
+def val_search(list_of_lists, val):
+    # ____Проходимо по елементам списку_____
+
+    for element in list_of_lists:           # Проходимо по елементам списку
+        if isinstance(element, list):       # перевіряємо чи є там список всередині
+            if val_search(element, val):    # якщо є список -> рекурсивно заходимо в нього
+                return True                 # якщо в ході рекурсії вернеться True - то знайшли значення і вертаємо True
+        else:
+            if element == val:              # порівнюємо int-елементи списку з val і шукаємо чи є таке значення
+                return True
+    #________________________________________
+
+    return False                            # якщо нічого не = val, то в кінці-кінців вертаємо False
+
 """ test functions """
 
 #print_num(10)
 #check_pow_2(16)
-print(num_sum(125))
+#print(num_sum(125))
+#print_1_to_n(5)
+#print(list_generator(n=5, num_list=[]))
+#res = list_generator2(5)
+#print(res)
+
+
+k = [1, [1,2,3], 5, [1, 2, [5, 6, [7]]]]
+val = 6
+
+print(val_search(k, val))
+
+
+
+
+
+
+
+
